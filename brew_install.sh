fd1c549dc687cce2fe0f7688a8674ee7c4bb9575
tieda <tieda@tieda-imac.local>
2020-08-02 01:38:40 -0400

    Add brew install

---
+++ brew_install.sh
@@ -0,0 +1,17 @@
+brew cask install appcleaner
+brew cask install visual-studio-code
+brew cask install sourcetree
+brew cask install postman
+brew cask install chrome
+brew cask install alfred
+brew cask install bartender
+brew cask install iterm2
+brew cask install discord
+brew cask install openinterminal
+brew cask install karabiner-elements
+brew cask install adguard
+brew cask install reflector
+brew cask install fantastical
+brew cask install wechat
+
+brew install node
