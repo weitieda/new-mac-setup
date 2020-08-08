c00380584a8baa356b3ca87398fe761cf73ecc4b
Tieda Wei <weitieda@gmail.com>
2020-08-08 18:10:33 -0400

    add vscode theme

--- app_settings.sh
+++ app_settings.sh
@@ -18,3 +18,9 @@ cp Karabiner/karabiner.json ~/.config/karabiner
 # Xcode Dracula Theme
 wget "https://github.com/dracula/xcode/archive/master.zip" && unzip master.zip && rm master.zip && cp xcode-master/Dracula.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
+# VSCode Dracula Theme, manual select theme required post-install. VSCode -> File -> Preferences -> Color Theme
+git clone https://github.com/dracula/visual-studio-code.git ~/.vscode/extensions/theme-dracula
+cd ~/.vscode/extensions/theme-dracula
+npm install
+npm run build
+
