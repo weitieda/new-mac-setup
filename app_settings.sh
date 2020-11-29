435769db80db0cc94bfdc54c41e1dff410070a23
Tieda Wei <weitieda@gmail.com>
2020-11-29 00:31:36 -0500

    Update

--- app_settings.sh
+++ app_settings.sh
@@ -9,22 +9,25 @@
 
 cd ~/Download
 
-# increase KeyRepeat speed, 0 is fastest
+# Increase KeyRepeat speed, 0 is fastest
 defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Turn off macOS update notification
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -boolean FALSE
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # On My Zsh Setting
-# add syntax highlighting
+# Add syntax highlighting
 brew install zsh-syntax-highlighting
 echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-# install powerline
+# Install powerline
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # zsh auto suggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# add to `plugins=(zsh-autosuggestions)`
+# Add to `plugins=(zsh-autosuggestions)`
 
 # Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.
 
