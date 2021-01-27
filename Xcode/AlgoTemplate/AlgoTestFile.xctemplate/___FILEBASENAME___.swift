aaac0b794ba74c6d79bcb0a01f28462d0428f5ae
Tieda Wei <weitieda@gmail.com>
2021-01-27 18:46:14 -0500

    Add AlgoTemplates

---
+++ Xcode/AlgoTemplate/AlgoTestFile.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,20 @@
+//___FILEHEADER___
+
+import XCTest
+@testable import Algorithms
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+    
+    func test1() {
+            
+    }
+    
+    func test2() {
+            
+    }
+    
+    func test3() {
+            
+    }
+}
+
