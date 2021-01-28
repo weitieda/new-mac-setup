c16f4b67fd317673e6eacc829d9db7b9a451df48
Tieda Wei <weitieda@gmail.com>
2021-01-28 12:08:44 -0500

    Add AlgoQuestionTemplate

---
+++ Xcode/Templates/AlgoTemplate/AlgoSolutionFile.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,17 @@
+//___FILEHEADER___
+
+import XCTest
+
+/*
+ 
+ */
+
+final class ___FILEBASENAMEASIDENTIFIER___ {
+    
+    // t: O(), s: O()
+    static func solution() {
+        
+    }
+    
+}
+
